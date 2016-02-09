1d12a9098011747512004031e849f914e2486c74
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2016-02-09 10:21:50 -0500

    Parameterizing Predicate specs
    I want to bombard this data structure with its given coercions and be
    very clear about expected results.

--- spec/lib/milan/predicate_spec.rb
+++ spec/lib/milan/predicate_spec.rb
@@ -3,9 +3,22 @@ require 'milan/predicate'
 
 module Milan
   RSpec.describe Predicate do
-    subject { described_class.new(predicate: 'Hello', cardinality: 1) }
-    its(:to_h) { should eq(predicate: 'Hello', cardinality: 1, translation_key_fragment: 'Hello', param_key: 'Hello') }
-    its(:name) { should eq('Hello') }
-    its(:predicate) { should eq('Hello') }
+    [
+      {
+        given: { predicate: 'title' },
+        to_h: { predicate: 'title', cardinality: 'many', translation_key_fragment: 'title', param_key: 'title' }
+      }, {
+        given: { predicate: 'title', cardinality: 1, param_key: 'dc_title', translation_key_fragment: 'ulra.title' },
+        to_h: {
+          predicate: 'title', cardinality: 1, translation_key_fragment: 'ulra.title', param_key: 'dc_title'
+        }
+      }
+    ].each_with_index do |spec_config, index|
+      context "scenario ##{index} (#{spec_config.fetch(:given)})" do
+        subject { described_class.new(spec_config.fetch(:given)) }
+        its(:to_h) { should eq(spec_config.fetch(:to_h)) }
+        its(:name) { should eq(spec_config.fetch(:given).fetch(:predicate)) }
+      end
+    end
   end
 end
