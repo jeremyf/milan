bd8c0580a383e4ee4e575e6c745eb9ad6ae7721c
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2016-02-09 09:46:41 -0500

    Updating Milan::Predicate to have explicit attrs
    Ensuring that I'm capturing the explicit settings of the given
    predicate through the initialization process.

--- lib/milan/predicate.rb
+++ lib/milan/predicate.rb
@@ -3,28 +3,31 @@ require 'dry/equalizer'
 module Milan
   # Responsible for containing the definition of a Predicate.
   #
-  # This includes the name
+  # This includes:
+  # * the name of the predicate
+  # * the parameter key of the predicate (as used for HTML form submission)
   class Predicate
+    DEFAULT_CARDINALITY = "many".freeze
+
     include Dry::Equalizer(:to_h)
 
     def initialize(predicate:, **keywords)
       self.predicate = predicate
       self.keywords = keywords
+      self.param_key = keywords.fetch(:param_key, predicate)
+      self.translation_key_fragment = keywords.fetch(:translation_key_fragment, predicate)
+      self.cardinality = keywords.fetch(:cardinality, DEFAULT_CARDINALITY)
     end
 
     def to_h
-      keywords.merge(predicate: predicate)
+      { predicate: predicate, param_key: param_key, translation_key_fragment: translation_key_fragment, cardinality: cardinality }
     end
 
-    attr_reader :predicate, :keywords
+    attr_reader :predicate, :keywords, :translation_key_fragment, :cardinality, :param_key
     alias name predicate
-
-    def param_key
-      keywords.fetch(:param_key, predicate)
-    end
 
     private
 
-    attr_writer :predicate, :keywords
+    attr_writer :predicate, :keywords, :translation_key_fragment, :cardinality, :param_key
   end
 end
--- spec/lib/milan/predicate_spec.rb
+++ spec/lib/milan/predicate_spec.rb
@@ -4,7 +4,7 @@ require 'milan/predicate'
 module Milan
   RSpec.describe Predicate do
     subject { described_class.new(predicate: 'Hello', cardinality: 1) }
-    its(:to_h) { should eq(predicate: 'Hello', cardinality: 1) }
+    its(:to_h) { should eq(predicate: 'Hello', cardinality: 1, translation_key_fragment: 'Hello', param_key: 'Hello') }
     its(:name) { should eq('Hello') }
     its(:predicate) { should eq('Hello') }
 
@@ -14,6 +14,17 @@ module Milan
       end
       it 'uses the predicate if a param_key is not given' do
         expect(described_class.new(predicate: 'Hello', cardinality: 1).param_key).to eq('Hello')
+      end
+    end
+
+    context '#translation_key_fragment' do
+      it 'uses the translation_key_fragment if one is given' do
+        expect(
+          described_class.new(predicate: 'Hello', cardinality: 1, translation_key_fragment: 'hello_world').translation_key_fragment
+        ).to eq('hello_world')
+      end
+      it 'uses the predicate if a translation_key_fragment is not given' do
+        expect(described_class.new(predicate: 'Hello', cardinality: 1).translation_key_fragment).to eq('Hello')
       end
     end
   end
