c0cf650544709a06d2753b29c1b9ef8c16be0f70
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2016-02-09 10:12:44 -0500

    Removing chatter on the Predicate specs
    Flay did not approve the duplication and its less relevant given the
    specs.

--- spec/lib/milan/predicate_spec.rb
+++ spec/lib/milan/predicate_spec.rb
@@ -7,25 +7,5 @@ module Milan
     its(:to_h) { should eq(predicate: 'Hello', cardinality: 1, translation_key_fragment: 'Hello', param_key: 'Hello') }
     its(:name) { should eq('Hello') }
     its(:predicate) { should eq('Hello') }
-
-    context '#param_key' do
-      it 'uses the param_key if one is given' do
-        expect(described_class.new(predicate: 'Hello', cardinality: 1, param_key: 'hello_world').param_key).to eq('hello_world')
-      end
-      it 'uses the predicate if a param_key is not given' do
-        expect(described_class.new(predicate: 'Hello', cardinality: 1).param_key).to eq('Hello')
-      end
-    end
-
-    context '#translation_key_fragment' do
-      it 'uses the translation_key_fragment if one is given' do
-        expect(
-          described_class.new(predicate: 'Hello', cardinality: 1, translation_key_fragment: 'hello_world').translation_key_fragment
-        ).to eq('hello_world')
-      end
-      it 'uses the predicate if a translation_key_fragment is not given' do
-        expect(described_class.new(predicate: 'Hello', cardinality: 1).translation_key_fragment).to eq('Hello')
-      end
-    end
   end
 end
