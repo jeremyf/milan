afc3694deddbae7806f4e78bd1e76eb656c34a99
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2016-02-10 13:40:13 -0500

    Updating Predicate documentation
    Stating what is meant by each Predicate property

--- lib/milan/predicate.rb
+++ lib/milan/predicate.rb
@@ -5,10 +5,6 @@ require 'hanami/utils/string'
 module Milan
   # Responsible for containing the definition of a Predicate.
   #
-  # This includes:
-  # * the name of the predicate
-  # * the parameter key of the predicate (as used for HTML form submission)
-  #
   # @todo Factor the predicate translator into a separate object. Predicate should be a struct with little behavior. Its compressed out
   #   of convenience.
   class Predicate
@@ -29,6 +25,7 @@ module Milan
       self.type = keywords.fetch(:type, DEFAULT_TYPE)
     end
 
+    # There are a few aliased methods; I'm not including those as they are not a part of the constructor.
     def to_h
       {
         predicate: predicate, param_key: param_key, translation_key_fragment: translation_key_fragment, cardinality: cardinality,
@@ -36,7 +33,41 @@ module Milan
       }
     end
 
-    attr_reader :predicate, :keywords, :translation_key_fragment, :cardinality, :param_key
+    # The name by which you call the predicate.
+    #
+    # @return String
+    attr_reader :predicate
+
+    # When this predicate is translated into different contexts, what is the translation key fragment.
+    #
+    # @return String
+    # @see Milan::TranslationAssistant for more information
+    attr_reader :translation_key_fragment
+
+    # When we are working with values for this predicate what is the expected cardinality? Will we have 1, 2, or "many" entries?
+    #
+    # @return ["many", Number]
+    # @see DEFAULT_CARDINALITY
+    attr_reader :cardinality
+
+    # The input parameter key to be used when submitting a form.
+    #
+    # @example
+    #   # Given Predicate#param_key == 'title' then rendering the input would be analog to the following:
+    #
+    #   ```html
+    #   <input name="title">
+    #   ```
+    #
+    #   ```json
+    #   { "title": "Some Title" }
+    #   ```
+    #
+    #   # There is an assumption that the param_keys can be nested; In Rails forms are submitted with a scoping hash key derived from
+    #   # the model_name. So we might end up with `{ "book": { "title": "Some Title" } }`
+    #
+    # @return String
+    attr_reader :param_key
 
     # When coercing the object portion of the triple (<subject><predicate><object>), what is the expected type for this given object
     #
@@ -46,6 +77,11 @@ module Milan
     attr_reader :type
 
     alias name predicate
+
+    # @!attribute [r] attribute_method_name
+    #   @return [String] a string that can be used for the Ruby method name of this attribute.
+    #   @note attribute_method_name is an alias of param_key, but is something that I don't want to overload in implementation.
+    #     HTML input names are far more forgiving than Ruby method names.
     alias attribute_method_name param_key
 
     private
